6e072e6dff6adf3afe7be7301caabdccaf81abd3
Avishka-Shamendra <avishka199849@gmail.com>
2023-02-14 12:50:03 +0530

    GameScene - point value calculation when dart hit

--- Mora dARts/Assets/Scripts/Dart.cs
+++ Mora dARts/Assets/Scripts/Dart.cs
@@ -24,17 +24,17 @@ public class Dart : MonoBehaviour
 
     private static Dictionary<string, int> pointMap = new Dictionary<string, int>
     {
-        {"HitArea.001", 0}, {"HitArea.002", 0}, {"HitArea.003", 0}, {"HitArea.004", 0}, {"HitArea.005", 0}, {"HitArea.006", 0}, {"HitArea.007", 0}, {"HitArea.008", 0},
-        {"HitArea.009", 0}, {"HitArea.010", 0}, {"HitArea.011", 0}, {"HitArea.012", 0}, {"HitArea.013", 0}, {"HitArea.014", 0}, {"HitArea.015", 0}, {"HitArea.016", 0},
-        {"HitArea.017", 0}, {"HitArea.018", 0}, {"HitArea.019", 0}, {"HitArea.020", 0}, {"HitArea.021", 0}, {"HitArea.022", 0}, {"HitArea.023", 0}, {"HitArea.024", 0},
-        {"HitArea.025", 0}, {"HitArea.026", 0}, {"HitArea.027", 0}, {"HitArea.028", 0}, {"HitArea.029", 0}, {"HitArea.030", 0}, {"HitArea.031", 0}, {"HitArea.032", 0},
-        {"HitArea.033", 0}, {"HitArea.034", 0}, {"HitArea.035", 0}, {"HitArea.036", 0}, {"HitArea.037", 0}, {"HitArea.038", 0}, {"HitArea.039", 0}, {"HitArea.040", 0},
-        {"HitArea.041", 0}, {"HitArea.042", 0}, {"HitArea.043", 0}, {"HitArea.044", 0}, {"HitArea.045", 0}, {"HitArea.046", 0}, {"HitArea.047", 0}, {"HitArea.048", 0},
-        {"HitArea.049", 0}, {"HitArea.050", 0}, {"HitArea.051", 0}, {"HitArea.052", 0}, {"HitArea.053", 0}, {"HitArea.054", 0}, {"HitArea.055", 0}, {"HitArea.056", 0},
-        {"HitArea.057", 0}, {"HitArea.058", 0}, {"HitArea.059", 0}, {"HitArea.060", 0}, {"HitArea.061", 0}, {"HitArea.062", 0}, {"HitArea.063", 0}, {"HitArea.064", 0},
-        {"HitArea.065", 0}, {"HitArea.066", 0}, {"HitArea.067", 0}, {"HitArea.068", 0}, {"HitArea.069", 0}, {"HitArea.070", 0}, {"HitArea.071", 0}, {"HitArea.072", 0},
-        {"HitArea.073", 0}, {"HitArea.074", 0}, {"HitArea.075", 0}, {"HitArea.076", 0}, {"HitArea.077", 0}, {"HitArea.078", 0}, {"HitArea.079", 0}, {"Ring.007",0},
-        {"Ring.008",0}
+        {"HitArea.001", 1}, {"HitArea.002", 18}, {"HitArea.003", 4}, {"HitArea.004", 13}, {"HitArea.005", 6}, {"HitArea.006", 10}, {"HitArea.007", 15}, {"HitArea.008", 2},
+        {"HitArea.009", 3}, {"HitArea.010", 19}, {"HitArea.011", 7}, {"HitArea.012", 16}, {"HitArea.013", 8}, {"HitArea.014", 11}, {"HitArea.015", 14}, {"HitArea.016", 9},
+        {"HitArea.017", 12}, {"HitArea.018", 5}, {"HitArea.019", 20}, {"HitArea.020", 1}, {"HitArea.021", 18}, {"HitArea.022", 4}, {"HitArea.023", 13}, {"HitArea.024", 6},
+        {"HitArea.025", 10}, {"HitArea.026", 15}, {"HitArea.027", 2}, {"HitArea.028", 17}, {"HitArea.029", 3}, {"HitArea.030", 19}, {"HitArea.031", 7}, {"HitArea.032", 16},
+        {"HitArea.033", 8}, {"HitArea.034", 11}, {"HitArea.035", 14}, {"HitArea.036", 9}, {"HitArea.037", 12}, {"HitArea.038", 5}, {"HitArea.039", 3*20}, {"HitArea.040", 3*1},
+        {"HitArea.041", 3*18}, {"HitArea.042", 3*4}, {"HitArea.043", 3*13}, {"HitArea.044", 3*6}, {"HitArea.045", 3*10}, {"HitArea.046", 3*15}, {"HitArea.047", 3*2}, {"HitArea.048", 3*17},
+        {"HitArea.049", 3*3}, {"HitArea.050", 3*19}, {"HitArea.051", 3*7}, {"HitArea.052", 3*16}, {"HitArea.053", 3*8}, {"HitArea.054", 3*11}, {"HitArea.055", 3*14}, {"HitArea.056", 3*9},
+        {"HitArea.057", 3*12}, {"HitArea.058", 3*5}, {"HitArea.059", 2*20}, {"HitArea.060", 2*1}, {"HitArea.061", 2*18}, {"HitArea.062", 2*4}, {"HitArea.063", 2*13}, {"HitArea.064", 2*6},
+        {"HitArea.065", 2*10}, {"HitArea.066", 2*15}, {"HitArea.067", 2*2}, {"HitArea.068", 2*17}, {"HitArea.069", 2*3}, {"HitArea.070", 2*19}, {"HitArea.071", 2*7}, {"HitArea.072", 2*16},
+        {"HitArea.073", 2*8}, {"HitArea.074", 2*11}, {"HitArea.075", 2*14}, {"HitArea.076", 2*9}, {"HitArea.077", 2*12}, {"HitArea.078", 2*5}, {"HitArea.079", 17}, {"HitArea.080", 20}, {"Ring.007",25},
+        {"Ring.008",50}
     };
 
     // Start is called before the first frame update
@@ -87,7 +87,7 @@ public class Dart : MonoBehaviour
     }
 
     
-
+    // Update point value
     private IEnumerator UpdatePointValue(string colliderName)
     {
         // Check if the point value is already being updated
@@ -95,16 +95,35 @@ public class Dart : MonoBehaviour
         {
             isUpdatingPointValueText = true; // Set the flag to indicate that the point value is currently being updated
             if (pointMap.ContainsKey(colliderName)){ // check if collider is valid
-                int remaining = int.Parse(pointValue.text) - pointMap[colliderName]; // get new point value
+                int pointMapVal =  pointMap[colliderName];
+                int remaining = int.Parse(pointValue.text) - pointMapVal; // get new point value
                 if (remaining > 0){ // if points are not zero yet
                     pointValue.text = remaining.ToString(); // update score
                     yield return new WaitForSeconds(0.1f); // Wait for 0.1 seconds to allow the point value to be updated smoothly
+                    StartCoroutine(ShowDartPointText(0.8f, pointMapVal)); //Show the score for the points score in that round
                 }
                 // TODO: remiander = 0 game won
                
             }
             isUpdatingPointValueText = false; // Reset the flag to indicate that the point value has finished updating
         }
+    }
+
+    // Method will display the points scored when a dart hits the board
+    IEnumerator ShowDartPointText(float duration,int points)
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+
+        TMP_Text dartPointText = GameObject.Instantiate(pointValue, canvas.transform); // get a copy of TMP_Text to display the score
+        dartPointText.fontSize = 180;
+        dartPointText.color = Color.magenta;
+        dartPointText.text = points.ToString(); // show the points
+        dartPointText.alignment = TextAlignmentOptions.Center;
+        dartPointText.rectTransform.localPosition = Vector3.zero;
+
+        yield return new WaitForSeconds(duration); // display text for given time
+
+        GameObject.Destroy(dartPointText.gameObject);
     }
 
     // When dart hits a collider, the follwing function is triggered
